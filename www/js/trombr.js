4ce476dfb8f274ca5919f18a61fc0ebc51cf15c6
Felipe Duarte <felipelageduarte@gmail.com>
2013-10-06 15:36:07 -0300

    Versao final 1.0.2

--- www/js/trombr.js
+++ www/js/trombr.js
@@ -1,4 +1,106 @@
-console.log('JavaScript Carregado com sucecsso!!!');
+var clima = -1;
+var latitude = $("#LatitudeTextInput").val();
+
+//preenche o menu de clima inicial
+loadMenu();
+function loadMenu(){
+	item = new String;	
+	item += '<label for="climasSelectMenu" class="select">Selecione o Clima:</label>';
+	item += '<select name="climasSelectMenu" id="climasSelectMenu">';
+	if(latitude < -23.5){
+		if(clima > 3 || clima < 1){
+			item += '<option value=-1 selected>Selecione o Clima de sua preferência</option>';
+			clima = -1;
+		}
+		item += '<option value=1 ' + ((clima == 1) ? "selected" : "") +'>Frio</option>';
+		item += '<option value=2 ' + ((clima == 2) ? "selected" : "") +'>Subtropical</option>';
+		item += '<option value=3 ' + ((clima == 3) ? "selected" : "") +'>Temperado</option>';
+	}else {
+		if(clima < 4){
+			item += '<option value=-1 selected>Selecione o Clima de sua preferência</option>';
+			clima = -1;
+		}		
+		item += '<option value=4 ' + ((clima == 4) ? "selected" : "") +'>Tropical de Altitude</option>';
+		item += '<option value=5 ' + ((clima == 5) ? "selected" : "") +'>Quente e Úmido</option>';
+		item += '<option value=6 ' + ((clima == 6) ? "selected" : "") +'>Quente e Seco</option>';
+		item += '<option value=7 ' + ((clima == 7) ? "selected" : "") +'>Continental</option>';
+	}
+	item += "</select>";
+	
+	$('#climaSelect').html(item).trigger("create");
+}
+
+$('#climaSelect').on('change', function(){
+	clima = $("#climasSelectMenu").val();
+	loadMenu();
+	clima = $("#climasSelectMenu").val();
+//	alert(clima);
+});
+
+$("#latitudeRange").on('change', function(){
+	latitude = $("#LatitudeTextInput").val();
+	loadMenu();
+});
+
+//callback para submeter o formulario
+$('#submit').on('click',function(){
+	
+	if(latitude < -33.77 || latitude > 4.5) {
+		alert("Latitude inválida. Forneça um valor no intervalor [-33.77,+4.5]");
+		return false;
+	}
+	
+	if(clima == -1){
+		alert("Selecione um clima válido");
+		return false;
+	}
+	
+	nextLocation = new String;
+	if(latitude < -23.5){
+		if(clima == 1){
+			nextLocation="./clima1.html";
+		}else if(clima == 2){
+			nextLocation="./clima2.html";
+		}else if(clima == 3){
+			nextLocation="./clima3.html";
+		}else{
+			alert("Selecione um clima válido");
+		return false;
+		}
+	}else if( latitude < -15){
+		if(clima == 4){
+			nextLocation="./clima4.html";
+		}else if(clima == 5){
+			nextLocation="./clima8.html";
+		}else if(clima == 6){
+			nextLocation="./clima7.html";
+		}else if(clima == 7){
+			nextLocation="./clima6.html";
+		}else{
+			alert("Selecione um clima válido");
+		return false;
+		}	
+	} else{
+		if(clima == 4){
+			nextLocation="./clima5.html";
+		}else if(clima == 5){
+			nextLocation="./clima11.html";
+		}else if(clima == 6){
+			nextLocation="./clima10.html";
+		}else if(clima == 7){
+			nextLocation="./clima9.html";
+		}else{
+			alert("Selecione um clima válido");
+		return false;
+		}	
+	}
+//	alert("lat: " + latitude + " | clima: " + clima + " | nextLocation: " + nextLocation);
+	$.mobile.changePage(nextLocation,{reverse:false,reloadPage:false,changeHash:true});
+	return false;
+});
+
+
+
 /*
 function resizeImg(){
 	var windowWidth = $(window).width();
@@ -24,114 +126,5 @@ function resizeImg(){
 
 $(window).load(resizeImg);
 $(window).resize(resizeImg);
-
-
-$(document).ready(function(){
-	// Grab a select field
-	var el = $('#climasSelectMenu');
-
-	// Select the relevant option, de-select any others
-	el.val('some value').attr('selected', true).siblings('option').removeAttr('selected');
-
-	// Initialize the selectmenu
-	el.selectmenu();
-	// jQM refresh
-	el.selectmenu("refresh", true);
-});
 */
-var clima = -1;
-/*
-function loadMenu(){
-	var latitude = $("#LatitudeTextInput").val();
-	item = new String;
-	
-	if(latitude < -23.5){	
-		item += '<option value=1 selected="selected">Frio</option>';
-		item += '<option value=2>Subtropical</option>';
-		item += '<option value=3>Temperado</option>';
-	}else {
-		item += '<option value=1 selected>Tropical de Altitude</option>';
-		item += '<option value=2>Quente e Úmido</option>';
-		item += '<option value=3>Quente e Seco</option>';
-		item += '<option value=4>Continental</option>';
-	}
-	$('#climasSelectMenu').html(item).trigger("create");
-}
 
-function submitCodigo(){
-	//validação do código inserido pelo usuário
-	var lat = document.forms["climaForm"]["LatitudeTextInput"].value;
-	
-	alert("RODOU");
-	
-	if(lat < -33.77 || lat > 4.5) {
-		alert("Latitude inválida. Forneça um valor no intervalor [-33.77,+4.5]");
-		return false;
-	}
-	
-	if(clima == -1){
-		alert("Selecione um clima válido");
-		return false;
-	}
-	
-	nextLocation = new String;
-	if(lat < -23.5){
-		if(clima == 1){
-			nextLocation="./clima1.html";
-		}else if(clima == 2){
-			nextLocation="./clima3.html";
-		}else if(clima == 3){
-			nextLocation="./clima2.html";
-		}else{
-			alert("Selecione um clima válido");
-		}
-	}else if( lat < -15){
-		if(clima == 1){
-			nextLocation="./clima4.html";
-		}else if(clima == 2){
-			nextLocation="./clima8.html";
-		}else if(clima == 3){
-			nextLocation="./clima7.html";
-		}else if(clima == 4){
-			nextLocation="./clima6.html";
-		}else{
-			alert("Selecione um clima válido");
-		}	
-	} else{
-		if(clima == 1){
-			nextLocation="./clima5.html";
-		}else if(clima == 2){
-			nextLocation="./clima11.html";
-		}else if(clima == 3){
-			nextLocation="./clima10.html";
-		}else if(clima == 4){
-			nextLocation="./clima9.html";
-		}else{
-			alert("Selecione um clima válido");
-		}	
-	}
-	console.log(lat);
-	console.log(clima);
-	console.log(nextLocation);
-	window.location=nextLocation;
-	return false;
-}
-
-$(document).on('change', function(){
-	loadMenu();
-});
-
-$(document).on('ready', function(){		
-	loadMenu();
-	
-	$('#climasSelectMenu').on('change', function(){
-		clima = $('#climasSelectMenu').val();
-	});
-
-	function isNumber(n) {
-	  return !isNaN(parseFloat(n)) && isFinite(n);
-	}
-	
-	$('#submit').on('click',submitCodigo);	
-});
-*/
